ea0f1a2b10a9d96e6fda07de0cb5e1434d77294f
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-05-02 15:43:57 +0300

    Update Whatsapp Number

--- src/Component/Whatsapp.jsx
+++ src/Component/Whatsapp.jsx
@@ -1,7 +1,7 @@
 function Whatsapp() {
   return (
     <div>
-      <a href="http://wa.me/+905512056862">
+      <a href="http://wa.me/+905055492481">
         <img
           src="https://i.pinimg.com/736x/93/b2/65/93b265c795140247db600ac92e58746a.jpg"
           width="60px"
